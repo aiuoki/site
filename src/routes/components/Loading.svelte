5f92343029c14dea5225c728df659e7ec9773f92
aiuoki <danielceban22@gmail.com>
2024-04-12 17:29:02 +0200

    minijuego

--- src/routes/components/Loading.svelte
+++ src/routes/components/Loading.svelte
@@ -1,4 +1,4 @@
-<div class="hero-content text-center text-neutral-content">
+<!-- <div class="hero-content text-center text-neutral-content">
 	<div class="max-w-md">
 		<span class="loading loading-spinner text-primary"></span>
 		<span class="loading loading-spinner text-secondary"></span>
@@ -9,4 +9,5 @@
 		<span class="loading loading-spinner text-warning"></span>
 		<span class="loading loading-spinner text-error"></span>
 	</div>
-</div>
+</div> -->
+<iframe src='https://www.minijuegos.com/embed/bike-mania' style='width:100%;height:100%;' frameborder='0' allowfullscreen></iframe>
